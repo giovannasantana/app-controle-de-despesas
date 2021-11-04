f17f2116824a52762c83a2dcfa457f803bf9fdf8
Giovanna <giovannamsantanaa@gmail.com>
2021-11-04 12:26:49 -0300

    Adicionando um array nas transacoes ficticias

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const inputTransactionAmount = document.querySelector('#amount');
 
 const dummyTransactions =[
     {id: 1, name: 'Bolo de brigadeiro', amount: -20}            //array
+    {id: 2, name: 'Salario', amount: 300}
+    {id: 3, name: 'Torta de frango', amount: 30}
+    {id: 4, name: 'Violao', amount: -500}
 ]
 
 const handleFormSubmit = event =>{
