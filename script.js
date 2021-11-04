0337af8b5d029f3a12c16e8a8e65674df58c8dc6
Giovanna <giovannamsantanaa@gmail.com>
2021-11-04 16:24:36 -0300

    Adicionando transacoes fisticias na lista

--- script.js
+++ script.js
@@ -3,18 +3,32 @@ const inputTransactionName = document.querySelector('#text');
 const inputTransactionAmount = document.querySelector('#amount');
 
 const dummyTransactions =[
-    {id: 1, name: 'Bolo de brigadeiro', amount: -20}            //array
-    {id: 2, name: 'Salario', amount: 300}
-    {id: 3, name: 'Torta de frango', amount: 30}
+    {id: 1, name: 'Bolo de brigadeiro', amount: -20},            //array
+    {id: 2, name: 'Salario', amount: 300},
+    {id: 3, name: 'Torta de frango', amount: 30},
     {id: 4, name: 'Violao', amount: -500}
 ]
 
 const handleFormSubmit = event =>{
     
-    if(inputTransactionName.ariaValueMax.trim() === '' || 
-            inputTransactionAmount.ariaValueMax.trim() === ''){
+    if(inputTransactionName.value.trim() === '' || 
+            inputTransactionAmount.value.trim() === ''){
         alert('Informe a descrição e o valor da transação');
     }
 }
 
 form.addEventListener('submit', handleFormSubmit);
+
+const addTransactionIntoDOM = transaction =>{
+    const li = document.createElement('li')
+
+    li.innerHTML = `${transaction.name}`
+    //atribuindo um nó para o li
+    transactionUl.append(li);
+}
+
+const init = () => {
+    dummyTransactions.forEach(addTransactionIntoDOM);
+}
+
+init();
